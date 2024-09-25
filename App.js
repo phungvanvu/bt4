53fdda56cdb9c2938f1fdaf732d2b07a1c5c233a
phungvanvu <phungvanvu0@gmail.com>
2024-09-25 17:45:01 +0700

    bt4

--- App.js
+++ App.js
@@ -1,20 +1,129 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+      backgroundColor: '#fff',
+      titleColor: 'black',
+    };
+  }
+
+  handleButtonClick = (newMessage, newColor, newTitleColor) => {
+    this.setState({ message: newMessage, backgroundColor: newColor, titleColor: newTitleColor });
+    Alert.alert(newMessage);
+  };
+
+  render() {
+    return (
+      <View style={[styles.container, { backgroundColor: this.state.backgroundColor }]}>
+        <TitleComponent title="Choose a Color" color={this.state.titleColor} />
+        <ButtonComponent
+          backgroundColor="green"
+          message="Green button clicked!"
+          onClick={this.handleButtonClick}
+          colorChange="#32CD32" 
+          titleColor="green"
+        />
+        <ButtonComponent
+          backgroundColor="blue"
+          message="Blue button clicked!"
+          onClick={this.handleButtonClick}
+          colorChange="#0000FF"
+          titleColor="blue"
+        />
+        <ButtonComponent
+          backgroundColor="brown"
+          message="Brown button clicked!"
+          onClick={this.handleButtonClick}
+          colorChange="#A52A2A"
+          titleColor="brown"
+        />
+        <ButtonComponent
+          backgroundColor="yellow"
+          message="Yellow button clicked!"
+          onClick={this.handleButtonClick}
+          colorChange="#FFFF00"
+          titleColor="yellow"
+        />
+        <ButtonComponent
+          backgroundColor="red"
+          message="Red button clicked!"
+          onClick={this.handleButtonClick}
+          colorChange="#FF6347"
+          titleColor="red"
+        />
+        <ButtonComponent
+          backgroundColor="black"
+          message="Black button clicked!"
+          onClick={this.handleButtonClick}
+          colorChange="#000000"
+          titleColor="black"
+        />
+        {this.state.message && <Text style={styles.message}>{this.state.message}</Text>}
+      </View>
+    );
+  }
+}
+
+class TitleComponent extends Component {
+  render() {
+    const { title, color } = this.props;
+    return <Text style={[styles.title, { color: color }]}>{title}</Text>;
+  }
+}
+
+class ButtonComponent extends Component {
+  render() {
+    const { backgroundColor, message, onClick, colorChange, titleColor } = this.props;
+    return (
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: backgroundColor }]}
+        onPress={() => onClick(message, colorChange, titleColor)}
+      >
+        <Text style={[
+            styles.buttonText,
+            { color: backgroundColor === 'yellow' ? 'black' : 'white' }
+          ]}>
+          {message.split(' ')[0]} {}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#F5F5F5', 
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  button: {
+    paddingVertical: 15,
+    paddingHorizontal: 100,
+    borderRadius: 25,
+    marginVertical: 10,
+    width: '90%', 
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  message: {
+    marginTop: 30,
+    fontSize: 20,
+    color: 'gray',
   },
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,10 @@
+<p float="left">
+    <img src="A1.png" width="200" />
+    <img src="A2.png" width="200" />
+    <img src="A3.png" width="200" />
+    <img src="A4.png" width="200" />
+    <img src="A5.png" width="200" />
+    <img src="A6.png" width="200" />
+    <img src="A7.png" width="200" />
+    <img src="A8.png" width="200" />
+</p>
